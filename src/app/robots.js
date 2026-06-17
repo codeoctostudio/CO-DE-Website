6c51eaf615825dc3fdc9caf330c8c2f37ae1c421
codeoctostudio <codeoctostudio@gmail.com>
2026-06-17 12:12:50 +0700

    add robots and llms.txt

---
+++ src/app/robots.js
@@ -0,0 +1,9 @@
+export default function robots() {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://co-dewebsite.vercel.app/th/sitemap.xml',
+  }
+}
