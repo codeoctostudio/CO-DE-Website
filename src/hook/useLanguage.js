e11eab995545e3e3e99fb0b77270ab8d93b0d8f0
codeoctostudio <codeoctostudio@gmail.com>
2026-07-02 12:31:44 +0700

    Getout useSearchParams from useLanguage.js

--- src/hook/useLanguage.js
+++ src/hook/useLanguage.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { getDictionary } from "@/lib/dictionary";
 
 export const useLanguage = () => {
