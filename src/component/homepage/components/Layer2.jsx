45ed185cad1ac9915e4645a1eb556d6b3ad78597
codeoctostudio <codeoctostudio@gmail.com>
2026-08-08 16:25:49 +0700

    save

--- src/component/homepage/components/Layer2.jsx
+++ src/component/homepage/components/Layer2.jsx
@@ -107,7 +107,7 @@ const Layer2 = () => {
                 >
                   {dict.layer2_6_5}
                 </button>
-                <p className="text-center text-[0.1rem] leading-tight opacity-70 md:text-sm md:leading-normal">
+                <p className="text-center text-[0.7rem] leading-tight opacity-70 md:text-sm md:leading-normal">
                   ⚡{dict.layer2_6_5_1}<br />
                   💖{dict.layer2_6_6}
                 </p>
